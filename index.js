b2184b725ce8f41afd443065ed5db2c42510a415
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-10-26 16:47:27 +0600

    selected course api set

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
+})
+
 app.listen(port, () => {
     console.log('Server is running on port', port);
 })
